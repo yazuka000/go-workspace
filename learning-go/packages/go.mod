26eb2010a1c49ab7085d836e66c2ee3694ecfe74
yazuka000 <yazuka.000@gmail.com>
2022-09-20 14:42:27 +0900

    Fix go.mod

---
+++ learning-go/packages/go.mod
@@ -0,0 +1,3 @@
+module github.com/yazuka000/myniceprogram
+
+go 1.19
--- learning-go/packages/main.go
+++ learning-go/packages/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "log"
+import (
+	"github.com/yazuka000/myniceprogram/helpers"
+	"log"
+)
 
 func main() {
 	log.Println("Hello")
+
+	var myVar helpers.SomeType
+	myVar.TypeName = "Some name"
+	log.Println(myVar.TypeName)
 }
