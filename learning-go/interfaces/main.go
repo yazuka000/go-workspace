c8c965a474a395a2018c327184f70f4ae060600a
yazuka000 <yazuka.000@gmail.com>
2022-09-20 09:57:24 +0900

    Add interfaces directory

---
+++ learning-go/interfaces/main.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+type Animal interface {
+	Says() string
+	NumberOfLegs() int
+}
+
+type Dog struct {
+	Name string
+	Breed string
+}
+
+type Gorilla struct {
+	Name string
+	Color string
+	NumberOfTeeth int
+}
+
+func main()  {
+	dog := Dog{
+		Name: "Samson",
+		Breed: "German Shephered",
+	}
+
+	PrintInfo(&dog)
+
+	gorilla := Gorilla {
+		Name: "Jack",
+		Color: "grey",
+		NumberOfTeeth: 38,
+	}
+
+	PrintInfo(&gorilla)
+}
+
+func PrintInfo(a Animal) {
+	fmt.Println("This animal says", a.Says(), "and has", a.NumberOfLegs(), "legs")
+}
+
+func (d *Dog) Says() string {
+	return "Woof"
+}
+
+func (d *Dog) NumberOfLegs() int {
+	return 4
+}
+
+func (d *Gorilla) Says() string {
+	return "Ugh"
+}
+
+func (d *Gorilla) NumberOfLegs() int {
+	return 2
+}
