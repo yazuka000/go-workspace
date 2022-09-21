61b3420d356ca95d409f5f4d54862b5541fb2567
yazuka000 <yazuka.000@gmail.com>
2022-09-21 14:59:30 +0900

    Update hello-world (learned how to use http's request and response in golang)

--- hello-world/main.go
+++ hello-world/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// fmt.Println("Hello, World!")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		n, err := fmt.Fprintf(w, "Hello, world!")
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
+	})
+
+	_ = http.ListenAndServe(":8080", nil)
 }
