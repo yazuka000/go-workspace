ae61fbff0719c58ba6e5ddc52c26a2bfb059ad4c
yazuka000 <yazuka.000@gmail.com>
2022-09-21 10:50:05 +0900

    Add some comment and review

--- learning-go/channels/helpers/helpers.go
+++ learning-go/channels/helpers/helpers.go
@@ -6,7 +6,12 @@ import (
 )
 
 func RandNum(n int) int {
+	// rand.Seedは、乱数の元となる情報もランダム化して再定義できる
+	// rand.Seedなしの場合、
+	// rand.Intnは乱数生成のプログラムなのに、一番最初に出力した値しか出力しなくなる
 	rand.Seed(time.Now().UnixNano())
+
+	// valueにランダムな値を格納
 	value := rand.Intn(n)
 	return value
 }
--- learning-go/channels/main.go
+++ learning-go/channels/main.go
@@ -9,16 +9,23 @@ const numPool = 1000
 
 func CalculateValue(intChan chan int) {
 	randomNumber := helpers.RandNum(numPool)
+
+	// チャネルに値を送信
 	intChan <- randomNumber
 }
 
 func main() {
-	intChan := make(chan int)
+	// 新しいチャネルを作成
+	intChan := make(chan int, 1)
+
+	// deferで呼び出した関数は、どこに定義されても一番最後に呼び出される。つまりfinally
+	// 下の例は、チャネルintChanをcloseし、値を送受信しないようにすることで、余計な誤作動を避けている
 	defer close(intChan)
 
 	// ゴルーチン
 	go CalculateValue(intChan)
 
+	// チャネルにストックされた値をnumに格納
 	num := <-intChan
 	log.Println(num)
 }
