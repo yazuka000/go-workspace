9b3b21f7c41d51b65ec4731b6f58591240eda9f7
yazuka000 <yazuka.000@gmail.com>
2022-09-22 09:49:10 +0900

    Add Home and About page in hello-world

--- hello-world/main.go
+++ hello-world/main.go
@@ -5,16 +5,31 @@ import (
 	"net/http"
 )
 
+var portNumber = ":8080"
+
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	// fmt.Fprintf(w, "Hello, world!")
+	fmt.Fprintf(w, "This is the home page")
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
+}
+
+// プライベートな関数は、小文字から定義する？
+// addValues adds two integers and return the sum
+func addValues(x, y int) int {
+	return x + y
+}
+
+// main is the main application function
 func main() {
-	// fmt.Println("Hello, World!")
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		n, err := fmt.Fprintf(w, "Hello, world!")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-	})
-
-	_ = http.ListenAndServe(":8080", nil)
+	fmt.Println((fmt.Sprintf("Starting application on port %s", portNumber)))
+	_ = http.ListenAndServe(portNumber, nil)
 }
