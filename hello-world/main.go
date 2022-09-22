19ce8544770bcd08b07398f15af6f26862bd6bbb
yazuka000 <yazuka.000@gmail.com>
2022-09-22 10:32:36 +0900

    Add some template files and render to web

--- hello-world/main.go
+++ hello-world/main.go
@@ -1,55 +1,68 @@
 package main
 
 import (
-	"errors"
+	// "errors"
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 var portNumber = ":8080"
 
-// Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprintf(w, "Hello, world!")
-	fmt.Fprintf(w, "This is the home page")
+	// fmt.Fprintf(w, "This is the home page")
+
+	renderTemplate(w, "home.page.tmpl")
 }
 
-// About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	sum := addValues(2, 2)
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
+	// sum := addValues(2, 2)
+	// _, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
+
+	renderTemplate(w, "about.page.tmpl")
 }
 
-// addValues adds two integers and return the sum
-func addValues(x, y int) int {
-	return x + y
-}
-
-func Divide(w http.ResponseWriter, r *http.Request) {
-	f, err := divideValues(100.0, 0.0)
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
-		fmt.Fprintf(w, "Cannot divide by 0")
+		fmt.Println("error parsing template:", err)
 		return
 	}
-
-	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", 100.0, 10.0, f))
 }
 
-func divideValues(x, y float32) (float32, error) {
-	if y <= 0 {
-		err := errors.New("cannot divide by zero")
-		return 0, err
-	}
-	result := x / y
-	return result, nil
-}
 
-// main is the main application function
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
-	http.HandleFunc("/divide", Divide)
+	// http.HandleFunc("/divide", Divide)
 
 	fmt.Println((fmt.Sprintf("Starting application on port %s", portNumber)))
 	_ = http.ListenAndServe(portNumber, nil)
 }
+
+
+
+// func addValues(x, y int) int {
+// 	return x + y
+// }
+
+// func Divide(w http.ResponseWriter, r *http.Request) {
+// 	f, err := divideValues(100.0, 0.0)
+// 	if err != nil {
+// 		fmt.Fprintf(w, "Cannot divide by 0")
+// 		return
+// 	}
+
+// 	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", 100.0, 10.0, f))
+// }
+
+// func divideValues(x, y float32) (float32, error) {
+// 	if y <= 0 {
+// 		err := errors.New("cannot divide by zero")
+// 		return 0, err
+// 	}
+// 	result := x / y
+// 	return result, nil
+// }
