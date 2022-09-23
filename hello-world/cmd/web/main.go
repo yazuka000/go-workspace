91637313dab91ba3676282a8a3d6c01109920565
yazuka000 <yazuka.000@gmail.com>
2022-09-23 10:58:02 +0900

    Separate go files into cmd/web and pkg directories

--- hello-world/cmd/web/main.go
+++ hello-world/cmd/web/main.go
@@ -2,24 +2,20 @@ package main
 
 import (
 	"fmt"
+	"myapp/pkg/handlers"
 	"net/http"
 )
 
 var portNumber = ":8080"
 
-
-
-
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 	// http.HandleFunc("/divide", Divide)
 
 	fmt.Println((fmt.Sprintf("Starting application on port %s", portNumber)))
 	_ = http.ListenAndServe(portNumber, nil)
 }
-
-
 
 // func addValues(x, y int) int {
 // 	return x + y
--- hello-world/pkg/handlers/handlers.go
+++ hello-world/pkg/handlers/handlers.go
@@ -1,6 +1,7 @@
-package main
+package handlers
 
 import (
+	"myapp/pkg/render"
 	"net/http"
 )
 
@@ -8,12 +9,12 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprintf(w, "Hello, world!")
 	// fmt.Fprintf(w, "This is the home page")
 
-	renderTemplate(w, "home.page.tmpl")
+	render.RenderTemplate(w, "home.page.tmpl")
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
 	// sum := addValues(2, 2)
 	// _, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
 
-	renderTemplate(w, "about.page.tmpl")
+	render.RenderTemplate(w, "about.page.tmpl")
 }
