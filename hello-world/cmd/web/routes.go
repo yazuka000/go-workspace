ccfb276437c2e5188eaa2cc934076ea17df062e5
yazuka000 <yazuka.000@gmail.com>
2022-09-27 10:35:32 +0900

    Add chi module

--- hello-world/cmd/web/routes.go
+++ hello-world/cmd/web/routes.go
@@ -5,14 +5,23 @@ import (
 	"myapp/pkg/handlers"
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	// "github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	// mux := pat.New()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
