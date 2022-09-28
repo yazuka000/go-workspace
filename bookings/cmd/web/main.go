3225691ab56fc35572928644d8d24235ef7c53bf
yazuka000 <yazuka.000@gmail.com>
2022-09-29 08:58:30 +0900

    Initialize bookings project

---
+++ bookings/cmd/web/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"github.com/yazuka000/bookings/pkg/config"
+	"github.com/yazuka000/bookings/pkg/handlers"
+	"github.com/yazuka000/bookings/pkg/render"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+)
+
+var portNumber = ":8080"
+var app config.AppConfig
+var session *scs.SessionManager
+
+func main() {
+
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	fmt.Println((fmt.Sprintf("Starting application on port %s", portNumber)))
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
