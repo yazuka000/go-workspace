ce06e89f517350007cded6eb3f07a8d1bea0f217
yazuka000 <yazuka.000@gmail.com>
2022-10-16 10:23:36 +0900

    Add helpers package

--- bookings/cmd/web/main.go
+++ bookings/cmd/web/main.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/yazuka000/bookings/internal/config"
 	"github.com/yazuka000/bookings/internal/handlers"
+	"github.com/yazuka000/bookings/internal/helpers"
 	"github.com/yazuka000/bookings/internal/models"
 	"github.com/yazuka000/bookings/internal/render"
 
@@ -18,6 +20,8 @@ import (
 var portNumber = ":8080"
 var app config.AppConfig
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 func main() {
 	err := run()
@@ -42,6 +46,12 @@ func run() error {
 	// change this to true when in production
 	app.InProduction = false
 
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
 	session.Cookie.Persist = true
@@ -64,6 +74,8 @@ func run() error {
 	handlers.NewHandlers(repo)
 
 	render.NewTemplates(&app)
+
+	helpers.NewHelpers(&app)
 
 	return nil
 }
