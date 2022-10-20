dc8330c57fa3caab8e44a3988713e833602382e9
yazuka000 <yazuka.000@gmail.com>
2022-10-20 10:10:09 +0900

    Add some migration files and execute soda migrate

--- bookings/migrations/schema.sql
+++ bookings/migrations/schema.sql
@@ -21,6 +21,160 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- Name: reservation; Type: TABLE; Schema: public; Owner: takahashikazuya
+--
+
+CREATE TABLE public.reservation (
+    id integer NOT NULL,
+    first_name character varying(255) DEFAULT ''::character varying NOT NULL,
+    last_name character varying(255) DEFAULT ''::character varying NOT NULL,
+    email character varying(255) NOT NULL,
+    start_date date NOT NULL,
+    end_date date NOT NULL,
+    phone character varying(255) DEFAULT ''::character varying NOT NULL,
+    room_id integer NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+ALTER TABLE public.reservation OWNER TO takahashikazuya;
+
+--
+-- Name: reservation_id_seq; Type: SEQUENCE; Schema: public; Owner: takahashikazuya
+--
+
+CREATE SEQUENCE public.reservation_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.reservation_id_seq OWNER TO takahashikazuya;
+
+--
+-- Name: reservation_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: takahashikazuya
+--
+
+ALTER SEQUENCE public.reservation_id_seq OWNED BY public.reservation.id;
+
+
+--
+-- Name: restrictions; Type: TABLE; Schema: public; Owner: takahashikazuya
+--
+
+CREATE TABLE public.restrictions (
+    id integer NOT NULL,
+    restriction_name character varying(255) DEFAULT ''::character varying NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+ALTER TABLE public.restrictions OWNER TO takahashikazuya;
+
+--
+-- Name: restrictions_id_seq; Type: SEQUENCE; Schema: public; Owner: takahashikazuya
+--
+
+CREATE SEQUENCE public.restrictions_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.restrictions_id_seq OWNER TO takahashikazuya;
+
+--
+-- Name: restrictions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: takahashikazuya
+--
+
+ALTER SEQUENCE public.restrictions_id_seq OWNED BY public.restrictions.id;
+
+
+--
+-- Name: room_restrictions; Type: TABLE; Schema: public; Owner: takahashikazuya
+--
+
+CREATE TABLE public.room_restrictions (
+    id integer NOT NULL,
+    start_date date NOT NULL,
+    end_date date NOT NULL,
+    room_id integer NOT NULL,
+    reservation_id integer NOT NULL,
+    restriction_id integer NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+ALTER TABLE public.room_restrictions OWNER TO takahashikazuya;
+
+--
+-- Name: room_restrictions_id_seq; Type: SEQUENCE; Schema: public; Owner: takahashikazuya
+--
+
+CREATE SEQUENCE public.room_restrictions_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.room_restrictions_id_seq OWNER TO takahashikazuya;
+
+--
+-- Name: room_restrictions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: takahashikazuya
+--
+
+ALTER SEQUENCE public.room_restrictions_id_seq OWNED BY public.room_restrictions.id;
+
+
+--
+-- Name: rooms; Type: TABLE; Schema: public; Owner: takahashikazuya
+--
+
+CREATE TABLE public.rooms (
+    id integer NOT NULL,
+    room_name character varying(255) DEFAULT ''::character varying NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+ALTER TABLE public.rooms OWNER TO takahashikazuya;
+
+--
+-- Name: rooms_id_seq; Type: SEQUENCE; Schema: public; Owner: takahashikazuya
+--
+
+CREATE SEQUENCE public.rooms_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.rooms_id_seq OWNER TO takahashikazuya;
+
+--
+-- Name: rooms_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: takahashikazuya
+--
+
+ALTER SEQUENCE public.rooms_id_seq OWNED BY public.rooms.id;
+
+
+--
 -- Name: schema_migration; Type: TABLE; Schema: public; Owner: takahashikazuya
 --
 
@@ -72,10 +226,70 @@ ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 
 --
+-- Name: reservation id; Type: DEFAULT; Schema: public; Owner: takahashikazuya
+--
+
+ALTER TABLE ONLY public.reservation ALTER COLUMN id SET DEFAULT nextval('public.reservation_id_seq'::regclass);
+
+
+--
+-- Name: restrictions id; Type: DEFAULT; Schema: public; Owner: takahashikazuya
+--
+
+ALTER TABLE ONLY public.restrictions ALTER COLUMN id SET DEFAULT nextval('public.restrictions_id_seq'::regclass);
+
+
+--
+-- Name: room_restrictions id; Type: DEFAULT; Schema: public; Owner: takahashikazuya
+--
+
+ALTER TABLE ONLY public.room_restrictions ALTER COLUMN id SET DEFAULT nextval('public.room_restrictions_id_seq'::regclass);
+
+
+--
+-- Name: rooms id; Type: DEFAULT; Schema: public; Owner: takahashikazuya
+--
+
+ALTER TABLE ONLY public.rooms ALTER COLUMN id SET DEFAULT nextval('public.rooms_id_seq'::regclass);
+
+
+--
 -- Name: users id; Type: DEFAULT; Schema: public; Owner: takahashikazuya
 --
 
 ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- Name: reservation reservation_pkey; Type: CONSTRAINT; Schema: public; Owner: takahashikazuya
+--
+
+ALTER TABLE ONLY public.reservation
+    ADD CONSTRAINT reservation_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: restrictions restrictions_pkey; Type: CONSTRAINT; Schema: public; Owner: takahashikazuya
+--
+
+ALTER TABLE ONLY public.restrictions
+    ADD CONSTRAINT restrictions_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: room_restrictions room_restrictions_pkey; Type: CONSTRAINT; Schema: public; Owner: takahashikazuya
+--
+
+ALTER TABLE ONLY public.room_restrictions
+    ADD CONSTRAINT room_restrictions_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: rooms rooms_pkey; Type: CONSTRAINT; Schema: public; Owner: takahashikazuya
+--
+
+ALTER TABLE ONLY public.rooms
+    ADD CONSTRAINT rooms_pkey PRIMARY KEY (id);
 
 
 --
