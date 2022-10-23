a7d0f7dd2e210d0f0ec6c194c5fc701c294fb58c
yazuka000 <yazuka.000@gmail.com>
2022-10-23 10:04:35 +0900

    Add some database model's struct in models.go

--- bookings/internal/models/models.go
+++ bookings/internal/models/models.go
@@ -1,9 +1,69 @@
 package models
 
+import "time"
+
 // Reservation holds reservation data
 type Reservation struct {
 	FirstName string
-	LastName string
-	Email string
-	Phone string
+	LastName  string
+	Email     string
+	Phone     string
+}
+
+// Users is the user model
+type Users struct {
+	ID          int
+	FirstName   string
+	LastName    string
+	Email       string
+	Password    string
+	AccessLevel int
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+// Rooms is the room model
+type Rooms struct {
+	Id        int
+	RoomName  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+// Restrictions is the restriction model
+type Restrictions struct {
+	Id              int
+	RestrictionName string
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+}
+
+// Reservations is the reservations model
+type Reservations struct {
+	Id        int
+	FirstName string
+	LastName  string
+	Email     string
+	Phone     string
+	StartDate time.Time
+	EndDate   time.Time
+	RoomId    int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Room      Rooms
+}
+
+// RoomRestrictions is the room restrictions model
+type RoomRestrictions struct {
+	Id            int
+	StartDate     time.Time
+	EndDate       time.Time
+	RoomId        int
+	ReservationId int
+	RestrictionId int
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	Room          Rooms
+	Reservation   Reservations
+	Restriction   Restrictions
 }
