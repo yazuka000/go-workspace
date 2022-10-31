9947bace127bb583c8e62e0fcaf96a69206c0f50
yazuka000 <yazuka.000@gmail.com>
2022-10-31 09:41:42 +0900

    Add test-repo.go and update some test files

---
+++ bookings/internal/repository/dbrepo/test-repo.go
@@ -0,0 +1,39 @@
+package dbrepo
+
+import (
+	"context"
+	"time"
+
+	"github.com/yazuka000/bookings/internal/models"
+)
+
+func (m *testDBRepo) AllUsers() bool {
+	return true
+}
+
+// InsertReservation inserts a reservation into the database
+func (m *testDBRepo) InsertReservation(res models.Reservation) (int, error) {
+	return 1, nil
+}
+
+// INsertRoomRestriction inserts a room restriction into the database
+func (m *testDBRepo) InsertRoomRestriction(r models.RoomRestriction) error {
+	return nil
+}
+
+// SearchAvailabilityByDates returns true if availability exists, and false if no availability exists
+func (m *testDBRepo) SearchAvailabilityByDatesByRoomId(start, end time.Time, roomId int) (bool, error) {
+	return false, nil
+}
+
+// SearchAvailabilityForAllRooms returns a slice of available rooms, if any, for given date range
+func (m *testDBRepo) SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error) {
+	var rooms []models.Room
+	return rooms, nil
+}
+
+// GetRoomById gets a room by id
+func (m *testDBRepo) GetRoomById(id int) (models.Room, error) {
+	var room models.Room
+	return room, nil
+}
