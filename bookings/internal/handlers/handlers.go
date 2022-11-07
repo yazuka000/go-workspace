9fdd163053c77d4a06bb0aa6f7e8bedae115e8a1
yazuka000 <yazuka.000@gmail.com>
2022-11-07 10:10:35 +0900

    Add sending notification to property owner

--- bookings/internal/handlers/handlers.go
+++ bookings/internal/handlers/handlers.go
@@ -211,6 +211,22 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 
 	m.App.MailChan <- msg
 
+	// send notification to property owner
+	htmlMessage = fmt.Sprintf(`
+	<strong>Reservation Confirmation</strong><br>
+	Dear %s:, <br>
+	This is confirm your reservation from %s to %s.
+`, reservation.FirstName, reservation.StartDate.Format("2006-01-02"), reservation.EndDate.Format("2006-01-02"))
+
+	msg = models.MailData{
+		To:      "me@here.com",
+		From:    "me@here.com",
+		Subject: "Reservation Confirmation",
+		Content: htmlMessage,
+	}
+
+	m.App.MailChan <- msg
+
 	m.App.Session.Put(r.Context(), "reservation", reservation)
 
 	http.Redirect(w, r, "/reservation-summary", http.StatusSeeOther)
