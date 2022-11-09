cebbf71398c690be7ef2ba5d34dbdde1af31c4db
yazuka000 <yazuka.000@gmail.com>
2022-11-09 11:03:10 +0900

    Add authentication and user database functions

--- bookings/internal/repository/repository.go
+++ bookings/internal/repository/repository.go
@@ -18,4 +18,10 @@ type DatabaseRepo interface {
 	SearchAvailabilityForAllRooms(start, end time.Time)([]models.Room, error)
 
 	GetRoomById(id int) (models.Room, error)
+
+	GetUserById(id int)(models.User, error)
+
+	UpdateUser(u models.User) error
+
+	Authenticate(email, testPassword string) (int, string, error)
 }
