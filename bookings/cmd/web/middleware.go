dcff7f141b7a35cdad070063adb272e78060120a
yazuka000 <yazuka.000@gmail.com>
2022-11-11 11:07:49 +0900

    Add Authentication middleware in middleware.go and helpers.go

--- bookings/cmd/web/middleware.go
+++ bookings/cmd/web/middleware.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/justinas/nosurf"
+	"github.com/yazuka000/bookings/internal/helpers"
 )
 
 func WriteToConsole(next http.Handler) http.Handler {
@@ -30,4 +31,15 @@ func NoSurf(next http.Handler) http.Handler {
 // SessionLoad loads and saves the session on every request
 func SessionLoad(next http.Handler) http.Handler {
 	return session.LoadAndSave(next)
+}
+
+func Auth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		if !helpers.IsAuthenticated(r){
+			session.Put(r.Context(), "error", "Log in first")
+			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
