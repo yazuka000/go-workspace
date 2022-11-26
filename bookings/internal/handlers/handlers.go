11b72e60dac45cc20d86d53de831f270de0ae67a
yazuka000 <yazuka.000@gmail.com>
2022-11-26 10:20:46 +0900

    Add AdminPostReservationCalendar'base function in handlers.go

--- bookings/internal/handlers/handlers.go
+++ bookings/internal/handlers/handlers.go
@@ -744,3 +744,8 @@ func (m *Repository) AdminDeleteReservation(w http.ResponseWriter, r *http.Reque
 	m.App.Session.Put(r.Context(), "flash", "Reservation deleted")
 	http.Redirect(w, r, fmt.Sprintf("/admin/reservations-%s", src), http.StatusSeeOther)
 }
+
+// AdminPostReservationCalendar handles post of reservation calendar
+func (m *Repository) AdminPostReservationCalendar(w http.ResponseWriter, r *http.Request) {
+	log.Println("Working")
+}
